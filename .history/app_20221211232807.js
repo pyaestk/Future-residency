1f9eb450c4c7ca467f49487d82b90274444389d0
Dylan <98664956+pyaestk@users.noreply.github.com>
2022-12-12 00:48:58 +0630

    update

---
+++ .history/app_20221211232807.js
@@ -0,0 +1,124 @@
+// mobile navigation
+var navLinks = document.getElementById("nav-right");
+function showMenu() {
+    navLinks.style.right = "0"; 
+}
+function hideMenu() {
+    navLinks.style.right = "-100vw"; 
+}
+
+// index page's swiperJs
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    600: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    }
+  }
+});
+
+// sale page's swiper js
+var swiper = new Swiper(".saleSwiper", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".saleSwiper2", {
+    speed: 500,
+    parallax: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+// item swiper
+var swiper = new Swiper(".itemSwiper", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".itemSwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+/* on scroll animation */
+
+function reveal() {
+  var reveals = document.querySelectorAll("#reveal");
+  var reveals2 = document.querySelectorAll('#reveal2');
+  var reveals3 = document.querySelectorAll('#reveal3');
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 50;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+  for (var i = 0; i < reveals2.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals2[i].getBoundingClientRect().top;
+    var elementVisible = 50;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals2[i].classList.add("active2");
+    } else {
+      reveals2[i].classList.remove("active2");
+    }
+  }
+  for (var i = 0; i < reveals3.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals3[i].getBoundingClientRect().top;
+    var elementVisible = 50;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals3[i].classList.add("active3");
+    } else {
+      reveals3[i].classList.remove("active3");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+
